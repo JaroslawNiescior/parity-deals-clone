609b2b1ebe83c0c5fcda5cdd0012d9e27d6fdcea
Jarosław Nieściór <jaroslaw.niescior@gmail.com>
2025-01-30 18:59:52 +0100

    Add footer section with link groups to marketing page; LandingPage done!

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,6 +1,7 @@
 import PricingSection from '@/components/sections/Pricing';
 import TestimonialsSection from '@/components/sections/Testimonials';
 import HeroSection from '@/components/sections/Hero';
+import FooterSection from '@/components/sections/Footer';
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HeroSection />
       <TestimonialsSection />
       <PricingSection />
+      <FooterSection />
     </>
   );
 }
