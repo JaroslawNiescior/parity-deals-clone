7cab7f22a31bf1a81b4925cf82bda805dab30116
Jarosław Nieściór <jaroslaw.niescior@gmail.com>
2025-01-31 15:58:41 +0100

    Add dashboard layout with navigation bar and middleware for protected routes

---
+++ src/app/dashboard/_components/NavBar.tsx
@@ -0,0 +1,23 @@
+import BrandLogo from '@/components/BrandLogo';
+import Link from 'next/link';
+
+export default function NavBar() {
+  return (
+    <header className="flex py-4 shadow bg-background">
+      <nav className="container flex items-center gap-10">
+        <Link href="/dashboard" className="mr-auto">
+          <BrandLogo />
+        </Link>
+        <Link href="/products" className="mr-auto">
+          Products
+        </Link>
+        <Link href="/products" className="mr-auto">
+          Analytics
+        </Link>
+        <Link href="/products" className="mr-auto">
+          Subscriptions
+        </Link>
+      </nav>
+    </header>
+  );
+}
