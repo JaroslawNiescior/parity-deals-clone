1aba460a1ffdb31b8658bde30db867d2db174b07
Jarosław Nieściór <jaroslaw.niescior@gmail.com>
2025-02-02 22:54:54 +0100

    Add NoProducts component and NewProductPage for product creation flow

---
+++ src/app/dashboard/products/new/page.tsx
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import PageWithBackButton from '@/app/dashboard/_components/PageWithBackButton';
+import ProductDetailsForm from '@/app/dashboard/_components/forms/ProductDetailsForm';
+
+export default function NewProductPage() {
+  return (
+    <PageWithBackButton
+      backButtonHref="/dashboard/products"
+      pageTitle="Create Product"
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-xl">Product Details</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ProductDetailsForm />
+        </CardContent>
+      </Card>
+    </PageWithBackButton>
+  );
+}
